c018e09342122e18398e53f741ef6c01e3aed02d
Rodrigo de Barros <guimabarros01@gmail.com>
2019-01-08 14:17:07 -0200

    commit inicial

---
+++ Fisica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucaoConflito
+{
+    public class Fisica
+    {
+        public double VelocidadeMedia(double distanciaPercorrida, double intervaloDeTempo)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Matematica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucaoConflito
+{
+    public class Matematica
+    {
+
+        public double Soma(double valor1, double valor2)
+        {
+            return 0;
+        }
+
+        public double Subtracao(double valor1, double valor2)
+        {
+            return 0;
+        }
+
+        public double Multiplicacao(double valor1, double valor2)
+        {
+            return 0;
+        }
+        
+        public double Divisao(double valor1, double valor2)
+        {
+            return 0;
+        }
+    }
+}
